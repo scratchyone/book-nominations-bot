1bf48a72bcb1e43e86e27ee84dfa7ad663710066
scratchyone <root@rachel.systems>
2026-05-18 21:20:04 -0400

    reactions

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ async def on_raw_reaction_add(reaction_: discord.RawReactionActionEvent):
                 discord.TextChannel, reaction_.channel_id
             )
             self.message = await self.channel.fetch_message(reaction_.message_id)
-            print("message: self.message")
+            print(f"message: {self.message}")
 
     reaction = Reaction()
     await reaction._init()
